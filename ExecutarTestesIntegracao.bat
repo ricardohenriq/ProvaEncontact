d3a47078172d85c4824859c41d670400132d1a76
Ricardo <ricardohenrique1@outlook.com>
2020-03-16 21:40:56 -0300

    - Inserido um "pause" ao termino do bat de execução dos testes, para que o avaliador possa verificar os resultados dos testes

--- ExecutarTestesIntegracao.bat
+++ ExecutarTestesIntegracao.bat
@@ -2,3 +2,4 @@ dotnet restore
 dotnet clean
 dotnet build
 dotnet test ./Prova.EnContact.TesteIntegracao/Prova.EnContact.TesteIntegracao.csproj --logger trx
+pause
--- ExecutarTestesUnitario.bat
+++ ExecutarTestesUnitario.bat
@@ -2,3 +2,4 @@ dotnet restore
 dotnet clean
 dotnet build
 dotnet test ./Prova.EnContact.TesteUnitario/Prova.EnContact.TesteUnitario.csproj --logger trx
+pause
