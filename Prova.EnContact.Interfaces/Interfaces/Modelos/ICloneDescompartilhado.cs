699af763b53c501d55d9cb4615e6f195685dd322
Ricardo <ricardohenrique1@outlook.com>
2020-03-19 19:22:05 -0300

    - Adicionado arquivos ao repositório

---
+++ Prova.EnContact.Interfaces/Interfaces/Modelos/ICloneDescompartilhado.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prova.EnContact.Interfaces.Interfaces.Modelos
+{
+    public interface ICloneDescompartilhado<T> : ICloneable
+    {
+        T CloneDescompartilhado();
+    }
+}
---
+++ Prova.EnContact.TesteUnitario/Modelos/AgrupamentoTests.cs
@@ -0,0 +1,33 @@
+﻿using Prova.EnContact.Interfaces.Interfaces.Modelos;
+using Prova.EnContact.Modelos.Modelos;
+using Prova.EnContact.TesteUnitario.Utilitarios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Prova.EnContact.TesteUnitario.Modelos
+{
+    public class AgrupamentoTests
+    {
+        [Theory]
+        [ClassData(typeof(GeradorDeRecadosAgrupaveis))]
+        public void CriarAgrupamentoSucessoTest(Recado primeiroRecado, Recado segundoRecado)
+        {
+            var agrupamento = new Agrupamento();
+            agrupamento.AdicioneRecado(primeiroRecado);
+
+            Assert.True(agrupamento.RecadoPertenceAoAgrupamanto(segundoRecado));
+        }
+
+        [Theory]
+        [ClassData(typeof(GeradorDeRecadosNaoAgrupaveis))]
+        public void CriarAgrupamentoFalhaTest(Recado primeiroRecado, Recado segundoRecado)
+        {
+            var agrupamento = new Agrupamento();
+            agrupamento.AdicioneRecado(primeiroRecado);
+
+            Assert.False(agrupamento.RecadoPertenceAoAgrupamanto(segundoRecado));
+        }
+    }
+}
